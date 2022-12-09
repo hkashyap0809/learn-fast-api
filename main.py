0581713909fc96f82625b92427ad15951d41967c
hkashyap0809 <hkashyap0809@gmail.com>
2022-12-09 19:40:55 +0530

    adding-path-parameters

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 # importing FastAPI
 from fastapi import FastAPI
 
+
 # creating an instance of FastAPI
 app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data':{'name':'Harshit Kashyap'}}
+    return {'data':'bloag list'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id :int):
+    # fetch blog with id = id
+    return {'data':id}
+
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id = id
+    return {'data':{'comments':['abc','xyz','pqr']}}
